8c40cbde91f4d477be05ce3d91cfc9f75f7e03fc
Jnguyen615 <Jenniferannnguyen@live.com>
2022-12-10 14:04:39 -0700

    added console logs

--- variable.js
+++ variable.js
@@ -3,7 +3,7 @@ var email = "jenniferannnguyen@live.com"
 var location = "littleton"
 
 var age = 37
-var date = "December 10th 2022"
+var pets = 3
 var children = 2 
 
 var female = true
@@ -14,9 +14,18 @@ blueEyes //=> false
 children // => 1
 location // => Estes Park
 
-console.log(age)
-console.log(female)
+console.log(name)
+console.log(email)
 console.log(location)
+
+console.log(age)
+console.log(pets)
+console.log(chilren)
+
+console.log(female)
+console.log(native)
+console.log(blueEyes)
+
 
 
 
