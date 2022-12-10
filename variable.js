9bbb9b5000b6761a0fe4698f217568be295c4eef
Jnguyen615 <Jenniferannnguyen@live.com>
2022-12-10 10:39:48 -0700

    added more variables

--- variable.js
+++ variable.js
@@ -4,9 +4,11 @@ var location = "littleton"
 
 var age = 37
 var date = December 10th 2022
-var childrem = 2 
+var children = 2 
 
 var female = true
 var native = true 
-var blue eyes = true 
+var blueEyes = true
+
+
 
