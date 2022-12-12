89c8fe07987e5e27b3af241da2c9b70d15eff4aa
Jnguyen615 <Jenniferannnguyen@live.com>
2022-12-12 11:03:03 -0700

    added console.log

--- variables.js
+++ variables.js
@@ -10,3 +10,18 @@ var paidOff = true
 var TRD = true 
 var blackout = false 
 
+color = Red 
+mileage = 15000
+blackout = true 
+
+console.log(carMake)
+console.log(model)
+console.log(color)
+
+console.log(year)
+console.log(mileage)
+console.log(seats)
+
+console.log(paidOff)
+console.log(TRD)
+console.log(blackout)
